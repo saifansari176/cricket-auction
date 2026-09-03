4c31d2a3fa9d053c5129349269ec3b6f2342c89d
saif <ansarisaif176@gmail.com>
2026-09-03 19:01:33 +0530

    get start redirection

--- src/app/pages/public/home/home.component.ts
+++ src/app/pages/public/home/home.component.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { PublicHeaderComponent } from '../../../shared/public-header/public-header.component';
+import { AuthService } from '../../../core/services/auth.service';
 
-@Component({ selector: 'app-home', standalone: true, imports: [RouterLink, PublicHeaderComponent], templateUrl: './home.component.html', styleUrl: './home.component.scss' })
-export class HomeComponent {}
+@Component({ selector: 'app-home', standalone: true, imports: [PublicHeaderComponent], templateUrl: './home.component.html', styleUrl: './home.component.scss' })
+export class HomeComponent {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  async getStarted(): Promise<void> {
+    const user = await this.authService.waitForUser();
+    await this.router.navigate([user?.active ? '/dashboard' : '/login']);
+  }
+}
