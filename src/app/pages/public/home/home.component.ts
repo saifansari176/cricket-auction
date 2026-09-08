686e4575eb19909260972059eee277cdb832a163
saif <ansarisaif176@gmail.com>
2026-09-08 19:37:50 +0530

    seo contain changes

--- src/app/pages/public/home/home.component.ts
+++ src/app/pages/public/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { PublicHeaderComponent } from '../../../shared/public-header/public-header.component';
 import { AuthService } from '../../../core/services/auth.service';
 
-@Component({ selector: 'app-home', standalone: true, imports: [PublicHeaderComponent], templateUrl: './home.component.html', styleUrl: './home.component.scss' })
+@Component({ selector: 'app-home', standalone: true, imports: [RouterLink, PublicHeaderComponent], templateUrl: './home.component.html', styleUrl: './home.component.scss' })
 export class HomeComponent {
   constructor(private authService: AuthService, private router: Router) {}
 
